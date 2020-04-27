1a403092f3e934feb9d4cbf13edb22aec6148bcd
felipefariasdasilva <feliipefarias@outlook.com>
2020-04-27 18:04:31 -0300

    added service discovery

--- src/main/java/com/alura/microservicos/loja/loja/service/CompraService.java
+++ src/main/java/com/alura/microservicos/loja/loja/service/CompraService.java
@@ -5,18 +5,18 @@ import com.alura.microservicos.loja.loja.controller.dto.InfoFornecedorDTO;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class CompraService {
 
     public void realizaCompra(CompraDTO compraDTO){
         
         RestTemplate client = new RestTemplate();
-        
-        compraDTO.getEndereco().getEstado();
 
         ResponseEntity<InfoFornecedorDTO> exchange = 
-            client.exchange("http://localhost:8081/info/" + compraDTO.get getEndereco().getEstado(), 
+            client.exchange("http://localhost:8081/info/" + compraDTO.getEndereco().getEstado(), 
                 HttpMethod.GET, null, InfoFornecedorDTO.class);
 
             System.out.println(exchange.getBody().getEndereco());
